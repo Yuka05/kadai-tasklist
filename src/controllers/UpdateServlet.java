d6fa53e06bb43a363412bdd57fa2494be067f485
yuka.Ota <u-tan05@outlook.jp>
2021-03-09 16:30:14 +0900

    modified edit.jsp

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -1,5 +1,8 @@
+package controllers;
+
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -12,45 +15,49 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class DestroyServlet
+ * Servlet implementation class UpdateServlet
  */
-@WebServlet("/destroy")
-public class DestroyServlet extends HttpServlet {
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DestroyServlet() {
+    public UpdateServlet() {
         super();
     }
 
     /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if (_token != null && _token.equals(request.getSession().getId())) {
+        if(_token != null && _token.equals(request.getSession().getId())){
             EntityManager em = DBUtil.createEntityManager();
 
             // セッションスコープからタスクのIDを取得して
             // 該当のIDのタスク１件のみをデータベースから取得
-            Task t = em.find(Task.class, (Integer) (request.getSession().getAttribute("task_id")));
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
 
+            //フォームの内容を各フィールドに上書き
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setUpdated_at(currentTime); // 更新日時のみ上書き
+
+            // データベースを更新
             em.getTransaction().begin();
-            em.remove(t);
             em.getTransaction().commit();
             em.close();
 
             // セッションスコープ上の不要になったデータを削除
             request.getSession().removeAttribute("task_id");
 
-            // indexページへリダイレクト
+            //indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
-
         }
-
     }
+
 }
